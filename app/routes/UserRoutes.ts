8177119ba0e69b6f8fed4497b6649e5fc37cbb22
Caio Monteiro Paulino <caio.monteiro.paulino@gmail.com>
2019-08-18 18:54:53 -0300

    Crud de usuário
    POST
    PUT
    GET
    GET BY ID
    DELETE
    
    Falta realizar a autenticação e criptogria de senha.

--- app/routes/UserRoutes.ts
+++ app/routes/UserRoutes.ts
@@ -6,7 +6,10 @@ import UserController from '../controllers/UserController';
 const routes = Router();
 
 routes.get('/', UserController.get);
+routes.get('/:id', UserController.getById);
 routes.post('/', UserController.post);
 routes.delete('/:id', UserController.delete);
+routes.put('/:id', UserController.put);
+
 
 export default routes;
