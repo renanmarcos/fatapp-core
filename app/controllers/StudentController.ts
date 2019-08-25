ab1a29f12241c8e7b1e1734e093ca9cabc94a63e
MatheusAMF <theus.saopaulo12@gmail.com>
2019-08-25 13:15:09 -0300

    Create logic for StudentController (#5)

--- app/controllers/StudentController.ts
+++ app/controllers/StudentController.ts
@@ -1,26 +1,92 @@
 import { Request, Response } from 'express';
 import { Student } from '../models/Student';
 import { Course } from '../services/enums/CourseEnum';
+import ValidationContract from 'app/bin/validation';
 
 class StudentController {
-  public async index (req: Request, res: Response): Promise<Response> {
+  public async get (req: Request, res: Response): Promise<Response> {
     return res.json(await Student.find());
   }
 
-  public async store (req: Request, res: Response): Promise<Response> {
-    const student = new Student();
-    student.ra = 123454;
-    student.course = Course.ADS;
-    await student.save();
-    
-    return res.json(student);
+  public async post (req: Request, res: Response) {
+    try {
+      let _validationContract = new ValidationContract();
+      //Validate required fields
+      _validationContract.isRequired(req.body.ra, 'RA is required');
+      _validationContract.isRequired(req.body.course, 'Course is required');
+      //Return bad request if validation fails.
+      if (!_validationContract.isValid()) {
+        res.status(400).send({
+          message: "400 Bad Request",
+          validation: _validationContract.errors()
+        }).end();
+        return res;
+      }
+      if(!Course.ADS.includes(req.body.ra)) {
+        res.status(400).send({
+          message: "Course not available",
+          validation: _validationContract.errors()
+        }).end();
+      }
+      
+      //If validation OK then create user.
+      const student = new Student();
+      student.ra = req.body.ra;
+      student.course = req.body.course;
+      await student.save();
+      //Return created student.
+      return res.json(student);
+    } catch (error) {
+      res.status(500).send({ message: '500 Internal Server Error', error: error })
+    }
   }
 
-  public async destroy (req: Request, res: Response): Promise<Response> {
-    const student = await Student.findOne({ id: 1 });
-    if (student != undefined) await student.remove();
+  //Return one and only one student by his id 
+  public async getById(req: Request, res: Response) {
+    try {
+      return res.json(await Student.findOne({ id: req.params.id }));
+    } catch (error) {
+      res.status(500).send({ message: '500 Internal Server Error', error: error })
+    }
+  }
 
-    return res.status(201);
+  //Remove student by id
+  public async delete(req: Request, res: Response) {
+    try {
+      let student = await Student.findOne({ id: req.params.id });
+      if (student) {
+        await student.remove();
+        return res.status(200).send('success');
+      } else {
+        res.status(404).send('404 Not Found');
+      }
+    } catch (error) {
+      res.status(500).send({ message: '500 Internal Server Error', error: error });
+    }
+  };
+
+  //Update student
+  public async put(req: Request, res: Response) {
+    try {
+      let student = await Student.findOne({ id: req.params.id });
+      if (student) {
+
+        //Avoid null or undefined params.
+        if (req.body.ra == undefined || req.body.ra == null) req.body.ra = student.ra;
+        if (req.body.course == undefined || req.body.course == null) req.body.course = student.course;
+        
+        //Update user and return the result
+        await Student.update(req.params.id, { ra: req.body.ra, course: req.body.course });
+        let updatedStudent = await Student.findOne({ id: req.params.id });
+        return res.status(200).send(updatedStudent);
+
+      } else {
+        res.status(404).send('404 Not Found');
+      }
+    } catch (error) {
+      res.status(500).send({ message: '500 Internal Server Error', error: error });
+    }
+
   }
 }
 
--- app/routes/StudentRoutes.ts
+++ app/routes/StudentRoutes.ts
@@ -5,8 +5,10 @@ import StudentController from '../controllers/StudentController';
 
 const routes = Router();
 
-routes.get('/', StudentController.index);
-routes.post('/', StudentController.store);
-routes.delete('/', StudentController.destroy);
+routes.get('students/', StudentController.get);
+routes.get('students/:id', StudentController.getById);
+routes.post('students/', StudentController.post);
+routes.delete('students/:id', StudentController.delete);
+routes.put('students/:id', StudentController.put);
 
 export default routes;
