5714d815a1b2687edee97ee223c0b591b1ba4627
Renan Marcos Ferreira <renan.ferreira18@fatec.sp.gov.br>
2019-10-28 00:25:36 -0300

    Fixes

--- app/services/CertificateGenerator.ts
+++ app/services/CertificateGenerator.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 import path from 'path';
 import { TemplateHandler } from 'easy-template-x';
 import { SendMail } from './mail/SendEmail';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 export class CertificateGenerator {
   
@@ -10,6 +12,7 @@ export class CertificateGenerator {
 
   constructor(subscription: Subscription) {
     this.subscription = subscription;
+    moment.locale('pt-BR');
   }
 
   public async sendCertificate() {
@@ -20,17 +23,8 @@ export class CertificateGenerator {
     let partialPath = path.join(__dirname, '../../storage/');
     let completePath = partialPath + event.certificate.path;
     const templateFile = fs.readFileSync(completePath);
-    
-    let diffInDate = activity.finalDate.getDate() - activity.initialDate.getDate();
-    let frequentedHours = activity.finalDate.getHours() - activity.initialDate.getHours();
 
-    if (diffInDate > 0 && frequentedHours == 0) {
-      frequentedHours = 24;
-    } 
-
-    if (diffInDate > 0) {
-      frequentedHours += diffInDate * 24;
-    }
+    let frequentedHours = moment(activity.finalDate).diff(activity.initialDate, 'hours');
 
     const data = {
       nome: user.name,
@@ -38,16 +32,19 @@ export class CertificateGenerator {
       palestrante: activity.speaker.speakerName,
       edicao: event.edition,
       evento: event.title,
-      dataAtividade: activity.initialDate.getDate(),
+      dataAtividade: moment(activity.initialDate).format("L"),
       cargaHoraria: frequentedHours + "h",
-      dataAtual: new Date().getDate()
+      dataAtual: moment().format("LL")
     };
     
     const handler = new TemplateHandler();
     const helper = new SendMail();
     helper.to = [user.email];
     helper.subject = "Certificado da atividade " + activity.title;
-    helper.text = "Agradecemos a sua participação na atividade " + activity.title + ". O certificado está em anexo.";
+    helper.text = "Olá " + user.name  + ".\nAgradecemos a sua participação na atividade " + 
+                  activity.title + "! Seu certificado está em anexo.\n\n" +
+                  "Esse e-mail é automático, por favor não responda.";
+
 
     handler.process(templateFile, data).then(function (generatedDocument: Buffer) {
       helper.attachment = {
